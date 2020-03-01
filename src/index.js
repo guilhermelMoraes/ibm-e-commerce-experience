a262fb9b3d3cd5df2a2f87949f2d4d8b418bb0be
Guilherme Leite Moraes <guilherme.lmoraes.devel@gmail.com>
2020-02-29 23:34:46 -0300

    added carbon components and ibm typography

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 import Router from './router';
 import './index.css';
 import './assets/fonts/open-sans/style.css';
+import 'carbon-components/css/carbon-components.css';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
